3df220ed052c3bab839d7e7e20f3d05a19d05066
elvxk <pradiptaramadan9c@gmail.com>
2025-05-28 06:40:52 +0700

    init(projects): initial projects smtp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="py-4 opacity-75">
+      <div className="text-center items-center justify-center flex flex-col">
+        <Link
+          className="flex flex-col justify-between items-center text-sm font-bold"
+          href="https://tools.pradipta.dev"
+          target="_blank"
+        >
+          <img
+            src="https://tools.pradipta.dev/eltoolnobg.webp"
+            alt="elvxk tool"
+            width={23}
+            height={23}
+          />
+          ELVXK TOOL
+        </Link>
+        <p className="text-sm">
+          Made with love © {new Date().getFullYear()}{" "}
+          <Link href="pradipta.dev" target="_blank">
+            elvxk
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
